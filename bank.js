55a2e8247f8fa7842052afb9e8333c3937f10676
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-02 20:37:19 -0300

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,11 @@
+console.log("Olá, mundo!");
+
+let nome=("Digite seu nome:");
+document.write(nome)
+
+let preco=("Digite seu preço:");
+document.write(preco)
+
+const cidade="Resende"
+console.log(cidade);
+
